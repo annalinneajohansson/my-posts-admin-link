7377ffec8fa76af6418e61f0069286b5c7d7878d
Anna <anna@hoxton.se>
2016-05-28 21:33:46 +0200

    Update my-posts-admin-link.php

--- my-posts-admin-link.php
+++ my-posts-admin-link.php
@@ -1,4 +1,46 @@
-<?php/*Plugin Name: My posts view linkPlugin URI: Description: Display a link to display only the logged in user posts in the edit posts view Version: 0.1Author: Anna JohanssonAuthor URI: http://annathewebdesigner.comText Domain: mypostslink */add_filter('current_screen', 'my_posts_view_get_current_screen' );function my_posts_view_get_current_screen( $screen ) {    if ( defined( 'DOING_AJAX' ) && DOING_AJAX )     	return $screen;	if ( $screen->base === 'edit' ) 		add_filter('views_'.$screen->id, 'my_posts_view_link' );    return $screen;} function my_posts_view_link( $links ) {	global $current_user;	global $wp_post_types;	$current_screen = get_current_screen();		$current_post_type = $current_screen->post_type;		$post_type_plural_name = $wp_post_types[$current_post_type]->labels->name;
-	$author = "author_name=$current_user->user_nicename";	$link = ( $current_post_type === 'post' ) ? "$current_screen->parent_file?$author" : "$current_screen->parent_file&amp;$author";		$output = "<a href=\"$link\">" . sprintf(__('My %s', 'mypostslink'), strtolower($post_type_plural_name)) . "</a>";   	$links['current-user-posts'] = $output;   	return $links;
-}
-function my_posts_view_init() {  load_plugin_textdomain( 'mypostslink', false, dirname( plugin_basename( __FILE__ ) ) ); }add_action('plugins_loaded', 'my_posts_view_init');
+<?php
+/*
+Plugin Name: "My posts" view link
+Plugin URI: https://github.com/annalinneajohansson/my-posts-admin-link/
+Description: Display a link to display only the logged in user posts in the edit posts view 
+Version: 0.1.1
+Author: Anna Johansson
+Author URI: http://annalinneajohansson.com
+Text Domain: mypostslink 
+*/
+add_filter('current_screen', 'my_posts_view_get_current_screen' );
+function my_posts_view_get_current_screen( $screen ) {
+    
+    if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+    	return $screen;
+    }
+    
+	if ( 'edit' === $screen->base ) {
+		add_filter('views_' . $screen->id, 'my_posts_view_link' );
+	}
+    return $screen;
+} 
+function my_posts_view_link( $links ) {
+	
+	global $current_user;
+	global $wp_post_types;
+
+	$current_screen = get_current_screen();	
+	$current_post_type = $current_screen->post_type;	
+	$post_type_plural_name = $wp_post_types[$current_post_type]->labels->name;
+
+	$author = "author_name=$current_user->user_nicename";
+	$link = ( $current_post_type === 'post' ) ? "$current_screen->parent_file?$author" : "$current_screen->parent_file&amp;$author";	
+
+	$output = "<a href=\"$link\">" . sprintf(__('My %s', 'mypostslink'), strtolower($post_type_plural_name)) . "</a>";
+
+   	$links['current-user-posts'] = $output;
+
+   	return $links;
+
+}
+
+function my_posts_view_init() {
+  load_plugin_textdomain( 'mypostslink', false, dirname( plugin_basename( __FILE__ ) ) ); 
+}
+add_action( 'plugins_loaded', 'my_posts_view_init' );
